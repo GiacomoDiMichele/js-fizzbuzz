6324b723d94cc2fe946539fad0468317096d3883
Giacomo <giacomo.1936@gmail.com>
2020-10-20 17:03:52 +0200

    invertita posizione della condizione "fizzbuzz"

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 //imposto un ciclo per far arrivare il numero da 1 fino a un max di 100
 for (var i = 1; i <= 100; i++) {
+//applico la condizione per la quale ogni multiplo sia di 3 che di 5 (se non danno resto) devono essere sostituiti da 'fizzbuzz'
+    if (i % 3 == 0 && i % 5 == 0) {
+        console.log('fizzBuzz');
+    }
 //applico la condizione per la quale ogni multiplo di 3 (se non da resto) deve essere sostituito da 'fizz'
-    if (i % 3 == 0) {
+    else if (i % 3 == 0) {
         console.log('fizz')
     }
 //applico la condizione per la quale ogni multiplo di 5 (se non da resto) deve essere sostituito da 'buzz'
     else if (i % 5 == 0) {
         console.log('buzz')
-    }
-//applico la condizione per la quale ogni multiplo sia di 3 che di 5 (se non danno resto) devono essere sostituiti da 'fizzbuzz'
-    else if (i % 3 && 5 == 0) {
-        console.log('fizzBuzz');
     }
 //altrimenti se i numeri danno resto nel calcolo stampo il numero
     else {
